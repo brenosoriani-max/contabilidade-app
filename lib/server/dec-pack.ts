0b92cedd567abd1fd043377eb0652d71b7e32a20
Breno Soriani - GTI <breno.soriani@contec.com>
2026-05-15 11:44:13 -0300

    feat: refactor DEC packing function to use AdmZip and remove ISO-8859-1 encoding handling

--- lib/server/dec-pack.ts
+++ lib/server/dec-pack.ts
@@ -1,27 +1,11 @@
-import JSZip from 'jszip';
-import iconv from 'iconv-lite';
+import AdmZip from 'adm-zip';
 
 export async function packDEC(xmlConteudo: string, nomeArquivoInterno = 'declaracao.xml') {
-  // 1. Ensure the XML header claims ISO-8859-1
-  let content = xmlConteudo;
-  if (content.includes('encoding="UTF-8"')) {
-    content = content.replace('encoding="UTF-8"', 'encoding="ISO-8859-1"');
-  } else if (!content.includes('encoding=')) {
-    // Inject encoding if missing
-    content = content.replace('?>', ' encoding="ISO-8859-1"?>');
-  }
+  const manifest = `<?xml version="1.0" encoding="UTF-8"?>\n<manifest xmlns="http://www.receita.fazenda.gov.br/manifest">\n    <file-entry\n        full-path="${nomeArquivoInterno}"\n        media-type="text/xml"/>\n</manifest>`;
 
-  // 2. Encode to ISO-8859-1 Buffer (Crucial for Brazilian tax software)
-  const xmlBuffer = iconv.encode(content, 'ISO-8859-1');
+  const zip = new AdmZip();
+  zip.addFile(nomeArquivoInterno, Buffer.from(xmlConteudo, 'utf8'));
+  zip.addFile('META-INF/manifest.xml', Buffer.from(manifest, 'utf8'));
 
-  // 3. Create standardized manifest
-  const manifest = `<?xml version="1.0" encoding="UTF-8"?><manifest xmlns="http://www.receita.fazenda.gov.br/manifest"><file-entry full-path="${nomeArquivoInterno}" media-type="text/xml"/></manifest>`;
-  
-  const zip = new JSZip();
-  zip.file(nomeArquivoInterno, xmlBuffer, { compression: 'DEFLATE' });
-  zip.file('META-INF/manifest.xml', manifest, { compression: 'DEFLATE' });
-
-  return Buffer.from(
-    await zip.generateAsync({ type: 'nodebuffer' })
-  );
+  return zip.toBuffer();
 }
